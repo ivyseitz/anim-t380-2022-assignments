25a6ba930e658c273d76b5fcc1c6e49eb13a8ffa
ivyseitz <ivyseitz@gmail.com>
2022-10-03 12:44:06 -0400

    Add files via upload
    working anim script, bounces ball

--- assignment-2/python/homework.py
+++ assignment-2/python/homework.py
@@ -1,24 +1,57 @@
 '''
 Version 0.01
 Last Edited: 10/3/2022
-Script Description: Launch Maya standalone, X, save Maya file
+Script Description: Launch Maya standalone, create bouncing ball anim, save Maya file
 '''
 
 
-#Import and launch Maya standalone
-import maya.standalone
-maya.standalone.initialize()
-
 #Import
+import maya.standalone
 import maya.cmds as cmds
 import argparse
 import os
 
-#Maya file
-print("Creating a cube...")
-maya.cmds.polyCube()
-print(maya.cmds.ls(geometry=True))
+#Get Arguements
+parser = argparse.ArgumentParser(description='This program makes a bouncing ball animation')
+parser.add_argument('ballSize', help='The radius of the ball')
+parser.add_argument('startHeight', help='Starting height of bouncing ball')
+args = parser.parse_args()
 
+#Initialize Maya
+maya.standalone.initialize()
+
+#Create Ball
+ball="bouncingBall"
+print("Creating a bouncy ball...")
+cmds.polySphere(name=ball, radius=args.ballSize)
+cmds.setAttr(ball+'.translateY',(int(args.ballSize)))
+cmds.makeIdentity(ball, apply=True, t=1)
+cmds.delete(ball, ch=True)
+cmds.setAttr(ball+'.translateY',(int(args.startHeight)))
+
+#Animate Ball
+bounce=int(args.startHeight)
+time = int(args.startHeight)
+interval = float(args.startHeight)
+cmds.setKeyframe(ball, attribute='translateY', t=0)
+
+while bounce > 1:
+    cmds.setAttr(ball+'.translateY',0)
+    cmds.setKeyframe(ball, attribute='translateY', t=time)
+    
+    bounce = float(bounce/2)
+    interval = interval/2
+    time += int(interval)
+    
+    #print("Bounce:",bounce)
+    #print("Interval:", interval)
+    #print("Time:",time)
+    
+    cmds.setAttr(ball+'.translateY',bounce)
+    cmds.setKeyframe(ball, attribute='translateY', t=time)
+    
+    time += int(interval)
+    
 
 #Save a Maya file to python script directory
 savePath= os.getcwd()
